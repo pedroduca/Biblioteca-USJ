e9fa39d7b3862140b873814b4edc645e24f18c55
Pedro Duca <testandoduca@gmail.com>
2019-06-02 01:24:36 -0300

    Started web system development from USJ library

---
+++ src/main/java/com/usj/biblioteca/BibliotecaUsjApplication.java
@@ -0,0 +1,13 @@
+package com.usj.biblioteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaUsjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaUsjApplication.class, args);
+	}
+
+}
